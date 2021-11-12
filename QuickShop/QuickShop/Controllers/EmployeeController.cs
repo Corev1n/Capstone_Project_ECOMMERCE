5315bb71bfc1239651bef1419764ec34b58409c9
Alfren James Cabuquit <aljamcabuquit@gmail.com>
2021-11-12 14:44:11 +0800

    main project

---
+++ QuickShop/QuickShop/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using QuickShop.Models;
+
+namespace QuickShop.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private ECommerceManagementDbContext employeeManagmentDbContext;
+        public EmployeeController(ECommerceManagementDbContext employeeManagmentDbContext) {
+            this.employeeManagmentDbContext = employeeManagmentDbContext;
+        }
+        
+        public IActionResult Index()
+        {
+            string[] breadcrumbs = { "Main", "Dashboard"};
+            ViewData["breadcrumbs"] = breadcrumbs;
+            return View();
+        }
+        public IActionResult New()
+        {
+            string[] breadcrumbs = { "Main", "User", "New User"};
+            ViewData["breadcrumbs"] = breadcrumbs;
+            return View();
+        }
+        public IActionResult Users()
+        {
+            string[] breadcrumbs = { "Main", "User", "Users" };
+            ViewData["breadcrumbs"] = breadcrumbs;
+            return View(employeeManagmentDbContext.Employees);
+        }
+        public IActionResult Activities()
+        {
+            string[] breadcrumbs = { "Main", "User", "Activities" };
+            ViewData["breadcrumbs"] = breadcrumbs;
+            return View();
+        }
+        public IActionResult Archives()
+        {
+            string[] breadcrumbs = { "Main", "User", "Archives" };
+            ViewData["breadcrumbs"] = breadcrumbs;
+            return View();
+        }
+    }
+}
---
+++ QuickShop/QuickShop/Migrations/20211112062406_InitialCreate.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuickShop.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    User_Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    User_Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image_Path = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Full_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Extension_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Mobile = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Customer_Status = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.User_Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    User_Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    User_Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image_Path = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Full_Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Extension_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Mobile = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Province = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.User_Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    User_Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    User_Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    User_Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    User_Type = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.User_Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}

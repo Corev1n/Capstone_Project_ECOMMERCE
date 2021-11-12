17b41182dc7a32b994bad0d6910c4887d652c336
Erick Beron <erickberon1@gmail.com>
2021-11-12 10:22:35 +0800

    Add Category Table

---
+++ ECommerce_Main/ECommerce_Main/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce_Main.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+        }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
